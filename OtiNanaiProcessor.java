22f30a3ff7bb11a3e092d10a9bd9c811513c1e33
Robert <rkrambovitis@phaistosnetworks.gr>
2013-06-21 16:29:37 +0300

    Before changing Process to return ArrayList

--- OtiNanaiProcessor.java
+++ OtiNanaiProcessor.java
@@ -11,12 +11,19 @@ class OtiNanaiProcessor {
 		onl = o;
 	}
 
+
 	public Vector<SomeRecord> processCommand(String input) {
 		storage = onl.getData();
+		dataMap = onl.getDataMap();
+		keyMaps = onl.getKeyMaps();
 		Vector<SomeRecord> matched = new Vector<SomeRecord>();
+		ArrayList<String> matchedIDs = new ArrayList<String>();
 		String[] inputWords = input.split("\\s");
 		String primary = inputWords[0];
-		matched = addWord(matched, primary);
+	//	matched = addWord(matched, primary);
+
+		matchedIDs = addWord(matchedIDs, primary);
+		
 		String word;
 		String rest;	
 		String firstChar = new String();
@@ -25,12 +32,25 @@ class OtiNanaiProcessor {
 			firstChar = word.substring(0,1);
 			rest = word.substring(1);
 			if (firstChar.equals("-")) {
-				matched = delWord(matched, rest);
+				//matched = delWord(matched, rest);
 			} else if (firstChar.equals("+")) {
-				matched = addWord(matched, rest);
+				//matched = addWord(matched, rest);
+				matchedIDs = addWord(matchedIDs, rest);
 			} 
 		}
 		return matched;
+	}
+
+	private ArrayList<String> addWord(ArrayList<String> mid, String key) {
+		if (keyMaps.containsKey(key)) 
+			mid.addAll(keyMaps.get(key));
+		return mid;
+	}
+
+	private ArrayList<String> delWord(ArrayList<String> mid, String key) {
+		if (keyMaps.containsKey(key)) 
+			mid.removeAll(keyMaps.get(key));
+		return mid;
 	}
 
 	private Vector<SomeRecord> addWord(Vector<SomeRecord> matched, String key) {
@@ -57,4 +77,6 @@ class OtiNanaiProcessor {
 
 	private OtiNanaiListener onl;
 	private CopyOnWriteArrayList<SomeRecord> storage;
+	private HashMap<String,SomeRecord> dataMap;
+	private HashMap<String,ArrayList<String>> keyMaps;
 }
