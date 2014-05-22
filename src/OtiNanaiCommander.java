c36ba7cd4fe58cdb24ec1ab82c95c2ed432e8b2b
Robert Krambovitis <rkrambovitis@phaistosnetworks.gr>
2014-05-22 14:44:08 +0300

    Lets see if this is lost

--- src/OtiNanaiCommander.java
+++ src/OtiNanaiCommander.java
@@ -3,8 +3,6 @@ package gr.phaistosnetworks.admin.otinanai;
 import java.io.*;
 import java.util.*;
 
-//Another pointless comment
-
 class OtiNanaiCommander implements Runnable {
 	public OtiNanaiCommander(OtiNanaiListener o) {
 		onp = new OtiNanaiProcessor(o);
