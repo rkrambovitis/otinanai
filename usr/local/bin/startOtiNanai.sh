bb9716164087e9d007c311e00c4eb2b9d87785d5
Robert Krambovitis <rkrambovitis@phaistosnetworks.gr>
2014-12-10 10:40:36 +0200

    remove old stuff

--- usr/local/bin/startOtiNanai.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export PATH=/bin:/sbin:/usr/bin:/usr/sbin
-
-cd /home/system/Tools/OtiNanai 
-#java -Xms16m -Xmx1024m OtiNanai -lf /tmp/otinanai.log -ll fine -wt 3 -lp 9876 -wp 9876 -ct 5 -ci 10 -al 3600 
-java -Xms16m -Xmx1024m -cp jars/riak-client-1.4.1.jar:OtiNanai gr.phaistosnetworks.admin.otinanai.OtiNanai
-
