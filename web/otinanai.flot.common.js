e17938869a22e77f8d0b30ee431dd78b908772a6
Robert Krambovitis <rkrambovitis@gmail.com>
2015-07-02 08:37:06 +0300

    prettier number formatting

--- web/otinanai.flot.common.js
+++ web/otinanai.flot.common.js
@@ -1,20 +1,24 @@
 function addSuffix(number) {
-   var suffix = "";
-   var result = number;
-   if (number > 500000000000) {
-      suffix = "P";
-      result = number / 1000000000000;
-   } else if (number > 500000000) {
-      suffix = "G";
-      result = number / 1000000000;
-   } else if (number > 500000) {
-      suffix = "M";
-      result = number / 1000000;
-   } else if (number > 500) {
-      suffix = "k";
-      result = number / 1000;
-   }
-   return result.toFixed(2)+suffix;
+        var suffix = "";
+        var result = number;
+        if (number > 500000000000) {
+                suffix = "P";
+                result = number / 1000000000000;
+        } else if (number > 500000000) {
+                suffix = "G";
+                result = number / 1000000000;
+        } else if (number > 500000) {
+                suffix = "M";
+                result = number / 1000000;
+        } else if (number > 500) {
+                suffix = "k";
+                result = number / 1000;
+        }
+
+        if (number < 10 )
+                result = result.toFixed(2);
+        else if (Math.abs(result) < 10 )
+                result = result.toFixed(1);
+
+        return result+suffix;
 };
-
-
