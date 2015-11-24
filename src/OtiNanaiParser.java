6756313ff07728186d9daa80f1ffa1bf63f355e9
Robert Krambovitis <rkrambovitis@gmail.com>
2015-11-24 11:38:00 +0200

    adding new file to git would help...

---
+++ src/OtiNanaiParser.java
@@ -0,0 +1,62 @@
+package gr.phaistosnetworks.admin.otinanai;
+
+import java.io.*;
+import java.util.*;
+import java.util.logging.*;
+import java.net.*;
+
+
+class OtiNanaiParser implements Runnable {
+	public OtiNanaiParser(OtiNanaiListener o, String td, InetAddress ip, Logger l) {
+		logger = l;
+		hip = ip;
+		theDato = td;
+		onl = o;
+	}
+
+	public void run() {
+		logger.finest("[Parser]: Attempting to parse: \""+theDato+"\" from "+hip);
+
+		SomeRecord newRecord = new SomeRecord(hip, theDato);
+		if (newRecord.isEvent()) {
+			onl.addEvent(newRecord);
+                        return;
+                }
+
+		ArrayList<String> theKeys = newRecord.getKeyWords();
+		for (String kw : theKeys) {
+			if (newRecord.isFreq()) {
+				try { 
+					Float.parseFloat(kw);
+					continue;
+				} catch (NumberFormatException e) {}
+
+				if (kw.equals("")) {
+					continue;
+				}
+			} 
+
+			KeyWordTracker kwt = onl.getKWT(kw);
+			if (kwt == null) {
+				logger.info("[Parser]: New KeyWord "+kw);
+				kwt = onl.addKWT(kw);
+			}
+
+			if (newRecord.isGauge()) {
+				kwt.putGauge(newRecord.getGauge());
+			} else if (newRecord.isSum()) {
+				kwt.putSum(newRecord.getSum());
+			} else if (newRecord.isCounter()) {
+				kwt.putCounter(newRecord.getCounter());
+			} else {
+				kwt.putFreq();
+			}
+		}
+	}
+
+	private String theDato;
+	private InetAddress hip;
+	private Logger logger;
+	private OtiNanaiListener onl;
+}
+
